a02ad6b737a6c6a3c7b50fb881e4dfca0dfe782d
Warren James <wajames@princeton.edu>
2020-07-21 15:26:49 -0400

    Updated api error-handling to be more robust and verbose

--- api/db.py
+++ api/db.py
@@ -2,30 +2,67 @@ from flask_pymongo import PyMongo
 import utils
 
 def set_db(app):
+    """
+    set global variable db_ to the db field of the current PyMongo instance
+    if this fails, raise a RuntimeError
+    """
     global db_
     db_ = PyMongo(app).db
     if db_ is None:
         raise RuntimeError("db_ not created")
 
 
-def get_by_state_and_date(stateAbbrv, date):
-    res = db_.covid_us.find_one(
-            {
-                "state_code": stateAbbrv.upper(),
-                "date": date
-            })
 
+def get_by_state_and_date(stateCode, date):
+    """
+    return document from db_.covid_us with 'state_code' field
+           that matches stateCode and 'date' field that matches date
+
+           if date is None, return most recent document
+           with 'state_code' field matching stateCode 
+    """
+    if not (date is None):
+        res = db_.covid_us.find_one(
+                {
+                    "state_code": stateCode.upper(),
+                    "date": date
+                })
+    else: # return most recent datapoint if date is None
+        res = db_.covid_us.aggregate(
+                [
+                    {"$match": {"state_code": stateCode.upper()}},
+                    {"$sort": {"date": -1}},
+                    {"$limit": 1}
+                ])
+        res = res.next()
     if res:
         res["_id"] = str(res["_id"])
         return res
     return utils.DOCUMENT_NOT_FOUND 
 
+
 def get_by_country_and_date(countryCode, date):
-    res = db_.covid_world.find_one(
-            {
-                "country_code": countryCode.upper(),
-                "date": date
-            })
+    """
+    return document from db_.covid_world with 'country_code' field
+           that matches countryCode and 'date' field that matches date
+
+           if date is None, return most recent document
+           with 'country_code' field matching countryCode
+    """
+    if (not date is None):
+        res = db_.covid_world.find_one(
+                {
+                    "country_code": countryCode.upper(),
+                    "date": date
+                })
+    else: # return most recent data point if date is None 
+        res = db_.covid_world.aggregate(
+                [
+                    {"$match": {"country_code": countryCode.upper()}},
+                    {"$sort": {"date": -1}},
+                    {"$limit": 1}
+                ])
+        res = res.next()
     if res:
         res["_id"] = str(res["_id"])
         return res
--- api/utils.py
+++ api/utils.py
@@ -1,12 +1,31 @@
 import datetime
 from flask import request
+from country_codes import COUNTRY_CODES
+import state_codes
 
+INVALID_STATE_STR ="Error! State code: <b>{}</b> is invalid" 
+INVALID_COUNTRY_STR ="Error! Country code: <b>{}</b> is invalid" 
 DOCUMENT_NOT_FOUND = {"error": "Document not found"}
 
 def get_date_from_args():
+    """
+    return -> datetime.datetime containing the date supplied in the url
+               if no date is supplied, return None
+    """
     date = request.args.get("date", default=None)
-    if date is None:
-        date = datetime.date.today()
-    else:
+    if not (date is None):
         date = datetime.datetime.strptime(date, "%d_%m_%Y")
     return date
+
+def validate_country_code(countryCode):
+    """
+    return True if countryCode is valid, False otherwise
+    """
+    return countryCode.upper() in COUNTRY_CODES
+
+
+def validate_state_code(stateCode):
+    """
+    return True if stateCode is valid, False otherwise
+    """
+    return stateCode.upper() in state_codes.STATE_CODES
