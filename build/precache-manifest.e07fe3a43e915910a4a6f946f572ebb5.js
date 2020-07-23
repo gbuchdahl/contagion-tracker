60a1b6fb3449cd574ddac62b2eefd25a10fd23ad
Gabriel Buchdahl <gbuchdahl@gmail.com>
2020-07-23 17:22:06 -0400

    production

---
+++ build/precache-manifest.e07fe3a43e915910a4a6f946f572ebb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696b57979721543a032c35d6628624bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac76fcb86a9e755b35d",
+    "url": "/static/css/main.db7b60d1.chunk.css"
+  },
+  {
+    "revision": "57cf8a4553204998b002",
+    "url": "/static/js/2.e79dfddb.chunk.js"
+  },
+  {
+    "revision": "e01eb0f37af85dfcc8900424d5ca88ea",
+    "url": "/static/js/2.e79dfddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac76fcb86a9e755b35d",
+    "url": "/static/js/main.a1d98197.chunk.js"
+  },
+  {
+    "revision": "8457acda92739ee9ef32",
+    "url": "/static/js/runtime-main.68102a0c.js"
+  }
+]);
